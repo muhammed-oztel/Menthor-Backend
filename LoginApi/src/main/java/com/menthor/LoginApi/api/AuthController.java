2bc086c11d4b19f3abc82184b815458252be0951
Dogukanguven <94847596+Dogukanguven@users.noreply.github.com>
2022-08-02 15:06:16 +0300

    Login_API
    userDetailsService ve passwordEncoder arasındaki cycle problemi devam etmekte.

---
+++ LoginApi/src/main/java/com/menthor/LoginApi/api/AuthController.java
@@ -0,0 +1,34 @@
+package com.menthor.LoginApi.api;
+
+import com.menthor.LoginApi.auth.TokenManager;
+import com.menthor.LoginApi.request.LoginRequest;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthController {
+    @Autowired
+    private TokenManager tokenManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+            return ResponseEntity.ok(tokenManager.generateToken(loginRequest.getUsername()));
+        }catch (Exception e){
+            throw e;
+        }
+
+    }
+}
---
+++ LoginApi/src/test/java/com/menthor/LoginApi/LoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menthor.LoginApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
