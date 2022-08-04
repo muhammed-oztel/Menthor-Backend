0d91c2a4941bd009292f25ff326a9a466bd13eb6
AhmetTuranMetin <ahmetturanmetin18@gmail.com>
2022-08-04 09:17:40 +0300

    event api create metods and match validation done

---
+++ MenthorProject/src/main/java/com/menthor/MenthorProject/service/EventService.java
@@ -0,0 +1,35 @@
+package com.menthor.MenthorProject.service;
+
+import com.menthor.MenthorProject.dto.UserDto;
+import com.menthor.MenthorProject.model.EventEntity;
+import com.menthor.MenthorProject.model.MatchEntity;
+import com.menthor.MenthorProject.repository.EventRepository;
+import com.menthor.MenthorProject.repository.MatchRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EventService {
+    private final EventRepository eventRepository;
+    private final MatchRepository matchRepository;
+    private final UserDto.Response response;
+
+    public EventService(EventRepository eventRepository, MatchRepository matchRepository) {
+        this.eventRepository = eventRepository;
+        this.matchRepository = matchRepository;
+        this.response = new UserDto.Response();
+    }
+
+    public UserDto.Response Create(EventEntity event){
+        Optional<MatchEntity> isEmpty = matchRepository.findById(event.getMatchId());
+        if (!isEmpty.isEmpty()){
+            eventRepository.save(event);
+            response.setMessage("Görüşme Kaydedildi.");
+            return response;
+        }else {
+            response.setMessage("Görüşme Kaydedilirken Bir Sorun Oluştu !");
+            return response;
+        }
+    }
+}
