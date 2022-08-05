39ec7dcc2cafdf5d6641df8fc881a8ee17cd66ad
oguzozturk-cmpe-ee <102048053+oguzozturk-cmpe-ee@users.noreply.github.com>
2022-08-05 10:49:46 +0300

    Calendar updated

--- menthor/src/main/java/com/menthor/repository/EventRepository.java
+++ menthor/src/main/java/com/menthor/repository/EventRepository.java
@@ -4,6 +4,9 @@ import com.menthor.model.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+    public List<EventEntity> findByMatchId(Long matchId);
 }
