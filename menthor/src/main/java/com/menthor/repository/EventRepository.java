3e38263259875d073d5a0e5ac2754f5c3aaf50dc
oguzozturk-cmpe-ee <102048053+oguzozturk-cmpe-ee@users.noreply.github.com>
2022-08-05 08:24:13 +0300

    Calendar added

---
+++ menthor/src/main/java/com/menthor/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.menthor.repository;
+
+import com.menthor.model.EventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<EventEntity, Long> {
+}
