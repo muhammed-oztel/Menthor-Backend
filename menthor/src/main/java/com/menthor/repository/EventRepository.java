17a42aa1efb145c81849024aa430a2031cdf481c
AhmetTuranMetin <ahmetturanmetin18@gmail.com>
2022-08-09 22:14:26 +0300

    event delete edited

--- menthor/src/main/java/com/menthor/repository/EventRepository.java
+++ menthor/src/main/java/com/menthor/repository/EventRepository.java
@@ -4,9 +4,10 @@ import com.menthor.model.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
-    public List<EventEntity> findByMatchId(Long matchId);
+    public List<EventEntity> findByMatchIdAndDeleted(Long matchId, Date deleted);
 }
