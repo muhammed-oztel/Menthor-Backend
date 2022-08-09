29208c0f42ee37f3b6638a040f130cda1a7aad37
oguzozturk-cmpe-ee <102048053+oguzozturk-cmpe-ee@users.noreply.github.com>
2022-08-09 16:33:49 +0300

    delete

--- menthor/src/main/java/com/menthor/repository/MeetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.menthor.repository;
-
-import com.menthor.model.MeetEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeetRepository extends JpaRepository<MeetEntity, Long> {
-}
--- menthor/src/main/java/com/menthor/service/UserServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.menthor.service;
-
-import com.menthor.model.UserEntity;
-
-public interface UserServiceInterface {
-    UserEntity validateUser(String email, String role, String password) throws Exception;
-}
