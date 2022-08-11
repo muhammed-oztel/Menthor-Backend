c3c3f0966d4c4f741425a7ccd6e8139a2bb7839b
AhmetTuranMetin <ahmetturanmetin18@gmail.com>
2022-08-11 17:00:02 +0300

    not match user list returned

--- menthor/src/main/java/com/menthor/service/AdminService.java
+++ menthor/src/main/java/com/menthor/service/AdminService.java
@@ -34,7 +34,20 @@ public class AdminService {
     }
 
     public List<UserEntity> AllUser(){
-        return userRepository.findAll();
+        List<UserEntity> allUser = userRepository.findAllByAndDeleted(null);
+//        List<MatchEntity> allMatch = matchRepository.findAllByAndDeleted(null);
+        for (int i=0;i<allUser.size();++i){
+            UserEntity user = userRepository.getReferenceById(allUser.get(i).getId());
+            List<MatchEntity> match = null;
+            if (user.getRole().toLowerCase().equals("mentor")){
+                match = matchRepository.findByMentorAndAndDeleted(allUser.get(i).getId(), null);
+            }else if (user.getRole().toLowerCase().equals("mentee")){
+                match = matchRepository.findByMenteeAndAndDeleted(allUser.get(i).getId(), null);
+            }
+            if (!match.isEmpty())
+                allUser.remove(i);
+        }
+        return allUser;
     }
 
     public List<UserDetailEntity> AllUserDetail(){
